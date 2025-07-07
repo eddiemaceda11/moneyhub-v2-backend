87c7945e38ccf66b0408a720c3225934611b9531
Eddie Maceda <eddiemaceda11@gmail.com>
2025-07-06 20:03:24 -0700

    Update the app.js file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const app = require('express')();
+const express = require('express');
+// const { PrismaClient } = require('@prisma/client');
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+const app = express();
+// const prisma = new PrismaClient();
+const { prisma } = require('./prismaClient');
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const transactions = await prisma.transaction.findMany({});
+
+  console.log(transactions);
+  res.end();
 });
 
-app.listen(3010, (req, res) => {
-  console.log('Server running on 3010');
+app.listen(3010, () => {
+  console.log('Server running on PORT 3010');
 });
